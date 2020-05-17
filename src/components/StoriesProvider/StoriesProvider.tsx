837e71d3aaea430b05e1cc6ffba299c6595c43b2
Pedro Ferrari <pedro@pedroferrari.com>
2020-05-17 19:05:42 +0200

    Add Roster Story

--- src/components/StoriesProvider/StoriesProvider.tsx
+++ src/components/StoriesProvider/StoriesProvider.tsx
@@ -10,6 +10,8 @@ import Theme from '../Styles/Theme';
 import GlobalStyles from '../Styles/GlobalStyles';
 import { ResetStyles } from '../Styles/ResetStyles';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 const GRAPHQL_SERVER = process.env.REACT_APP_GRAPHQL_ENDPOINT || 'http://localhost:4001/graphql';
 
 const client = new ApolloClient({
@@ -26,7 +28,9 @@ const StoriesProvider = ({ children }: { children: React.ReactNode; }) => {
         <ResetStyles />
         <GlobalStyles />
         <Theme>
-          {children}
+          <Router>
+            {children}
+          </Router>
         </Theme>
       </React.StrictMode>
     </ApolloProvider>
